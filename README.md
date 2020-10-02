6cb8c58a42cd465632fcf2f41b8bf8d9bfbec8b2
Param Siddharth <contact@paramsid.com>
2020-10-02 15:58:22 +0530

    Finalize version 1.1.0, Add README

---
+++ README.md
@@ -0,0 +1,53 @@
+# Imaginator
+An Express middleware that converts image tags to use data URIs instead of image files an HTML document.
+
+## Usage
+```
+const express = require('express');
+const path = require('path');
+const imaginator = require('imaginator');
+
+const app = express();
+
+app.use(imaginator(path.join(__dirname, 'public'), 'img.datauri'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000);
+```
+
+**`imaginator(publicDirectory, selector = 'img')`**
+
+- `publicDirectory` : The path to the directory where the hosted public files (including the images) are stored. In most cases, it should be set to `path.join(__dirname, 'public')`.
+ - _`selector`_ : The CSS-style selector for the tags to be affected. Its default value is `'img'` i. e. All `img` tags will be affected.
+
+# Example
+
+`index.html` before applying Imaginator:
+``` html
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Hi</title>
+	</head>
+	<body>
+		Hello world! <br/>
+		<img src='images/helloworld.png'>
+	</body>
+</html>
+```
+
+`index.html` after applying Imaginator:
+``` html
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Hi</title>
+	</head>
+	<body>
+		Hello world! <br/>
+		<img src='data:image/png;base64,...=='>
+	</body>
+</html>
+```
+
+Made with ❤ by Param.
--- index.js
+++ index.js
@@ -4,29 +4,38 @@ const fs = require('fs');
 const mimeTypes = require('mime-types');
 const htmlParser = require('node-html-parser').parse;
 
-
-module.exports = (publicDirectory) => modifyResponse((req, res) => typeof res.getHeader('Content-Type') == 'string' && res.getHeader('Content-Type').startsWith('text/html'),
-	(req, res, body) => {
-	let htmlDom = htmlParser(body);
-	if (htmlDom) {
-		let imgTags = htmlDom.querySelectorAll('img');
-		if (imgTags) {	
-			for (let imgTag of imgTags) {
-				if (!imgTag.getAttribute('src').startsWith('data:')) {
-					let reqPath = req.path;
-					while (reqPath[reqPath.length - 1] == '/')
-						reqPath = reqPath.substr(0, reqPath.length - 1);
-					reqPath = reqPath.substr(0, reqPath.lastIndexOf('/'))
-					let data = fs.readFileSync(path.join(publicDirectory, reqPath, imgTag.getAttribute('src')));
-					let fileType = mimeTypes.lookup(imgTag.getAttribute('src'));
-					fileType = (/\//.test(fileType)) ? fileType : `image/${fileType}`;
-					let dataB64 = (Buffer.isBuffer(data)) ? data.toString('base64') : Buffer.from(data).toString('base64');
-					let imgB64 = `data:${fileType};base64,${dataB64}`;
-					imgTag.setAttribute('src', imgB64);
+/**
+ * Converts image tags to use data URIs instead of image files an HTML document.
+ * @param {String} publicDirectory - The path to the directory where the hosted public files (including the images) are stored. For a static server, the recommended value is path.join(__dirname, 'public').
+ * @param {String} [selector=img] - The CSS-style selector for the image tags to be affected.
+ */
+function imaginator(publicDirectory, selector = 'img') 
+{
+	return modifyResponse((req, res) => typeof res.getHeader('Content-Type') == 'string' && res.getHeader('Content-Type').startsWith('text/html'),
+		(req, res, body) => {
+		let htmlDom = htmlParser(body);
+		if (htmlDom.valid) {
+			let imgTags = htmlDom.querySelectorAll(selector);
+			if (imgTags) {	
+				for (let imgTag of imgTags) {
+					if (!imgTag.getAttribute('src').startsWith('data:')) {
+						let reqPath = req.path;
+						while (reqPath[reqPath.length - 1] == '/')
+							reqPath = reqPath.substr(0, reqPath.length - 1);
+						reqPath = reqPath.substr(0, reqPath.lastIndexOf('/'))
+						let data = fs.readFileSync(path.join(publicDirectory, reqPath, imgTag.getAttribute('src')));
+						let fileType = mimeTypes.lookup(imgTag.getAttribute('src'));
+						fileType = (/\//.test(fileType)) ? fileType : `image/${fileType}`;
+						let dataB64 = (Buffer.isBuffer(data)) ? data.toString('base64') : Buffer.from(data).toString('base64');
+						let imgB64 = `data:${fileType};base64,${dataB64}`;
+						imgTag.setAttribute('src', imgB64);
+					}
 				}
+				return htmlDom.toString();
 			}
-			return htmlDom.toString();
 		}
-	}
-	return body;
-});
+		return body;
+	});
+}
+
+module.exports = imaginator;
